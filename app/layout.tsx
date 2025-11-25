5646b5356954f41bdc37a7edd01d95d6099aa9c6
Anuroop Srivastava <92581043+AnuroopSrivastava@users.noreply.github.com>
2025-11-25 20:19:41 +0530

    Import React in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
+import React from "react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
